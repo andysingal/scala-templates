6ec31edb8cc96230e704e33d3566f1e5703223ad
Ankush Singal <andysingal@gmail.com>
2025-02-27 16:44:25 -0600

    Add files via upload

---
+++ best-practices/matches/PatternMatchingGuards.scala
@@ -0,0 +1,18 @@
+import scala.util.Random
+
+@main
+def patternMatchingGuards(): Unit = {
+  val weekday = "Monday"
+  val isItAm = false
+  weekday match
+    case "Saturday" if isItAm => println("Sleep in")
+    case "Sunday" if isItAm => println("Do chores")
+    case "Saturday" | "Sunday" if !isItAm => println("Go out and have fun")
+    case _ => println("Go to work")
+
+  val number = Random.nextInt(20)
+  number match
+    case x if 0 until 10 contains number => println(s"Single digit $x")
+    case y if 10 until 20 contains number => println(s"Double digit $y")
+    case _ => println("Large number")
+}
